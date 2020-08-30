25b8abb4446dc2b2f6ba51e7e56de5f9f5644082
wangyinhao <luckywoniu@163.com>
2020-08-30 19:17:10 +0800

    update

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack')
+
+module.exports = {
+   entry: {
+     app: './src/index.js',
+    //  print: './src/print.js'
+   },
+   devtool:'inline-source-map',
+   devServer:{
+    contentBase:'./dist',
+    hot:true
+   },
+  output: {
+     filename: 'bundle.js',
+     filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins:[
+    //   new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({title:'Output  Management'}),
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin()
+  ]
+};
